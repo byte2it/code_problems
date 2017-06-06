41536c982508c2fa7f8fe83e6d981db7c64d4949
dolphinden <dolphinden007@gmail.com>
2017-06-06 11:52:45 -0400

    variable names corrected

--- LRU_cache.cpp
+++ LRU_cache.cpp
@@ -14,40 +14,40 @@ class LRUCache {
 		int value;
 		CacheEntry(int k, int v) : key(k), value(v) {}
 	};
-	unordered_map<int, list<CacheEntry>::iterator> _cache;
-	list<CacheEntry> _queue;
-	int _capacity;
+	unordered_map<int, list<CacheEntry>::iterator> cache;
+	list<CacheEntry> queue;
+	int capacity;
 public:
-	LRUCache(int capacity) :_capacity(capacity) {}
+	LRUCache(int cap) :capacity(cap) {}
 
 	int get(int key) {
-		if (_cache.find(key) == _cache.end())
+		if (cache.find(key) == cache.end())
 			return -1;
 
 		MoveToHead(key);
-		return _cache[key]->value;
+		return cache[key]->value;
 	}
 
 	void put(int key, int value) {
-		if (_cache.find(key) == _cache.end()) {
+		if (cache.find(key) == cache.end()) {
 			CacheEntry newItem(key, value);
-			if (_queue.size() >= _capacity) {
-				_cache.erase(_queue.back().key);
-				_queue.pop_back();
+			if (queue.size() >= capacity) {
+				cache.erase(queue.back().key);
+				queue.pop_back();
 			}
-			_queue.push_front(newItem);
-			_cache[key] = _queue.begin();
+			queue.push_front(newItem);
+			cache[key] = queue.begin();
 			return;
 		}
-		_cache[key]->value = value;
+		cache[key]->value = value;
 		MoveToHead(key);
 	}
 private:
 	void MoveToHead(int key) {
-		auto temp = *_cache[key];
-		_queue.erase(_cache[key]);
-		_queue.push_front(temp);
-		_cache[key] = _queue.begin();
+		auto temp = *cache[key];
+		queue.erase(cache[key]);
+		queue.push_front(temp);
+		cache[key] = queue.begin();
 	}
 };
 
