92dc50cdd89f7142a8a911085507bdc59e698f14
Denys Ponomarenko <dolphinden007@gmail.com>
2019-05-22 09:45:15 -0400

    tabs to spaces

--- cross_the_bridge.cpp
+++ cross_the_bridge.cpp
@@ -23,57 +23,57 @@ The approach is:
 
 int get_time(vector<int>& v) {
 
-	if (v.size() == 0)
-		return 0;
+    if (v.size() == 0)
+    return 0;
     
     if (v.size() == 2)
         return max(v[0], v[1]);
 
-	int res = 0;
+    int res = 0;
 
-	if (v.size() == 1 || v.size() == 3) {
-		for (int i = 0; i < v.size(); ++i)
-			res += v[i];
-		return res;
-	}
+    if (v.size() == 1 || v.size() == 3) {
+        for (int i = 0; i < v.size(); ++i)
+            res += v[i];
+        return res;
+    }
 
-	std::sort(v.begin(), v.end());
+    std::sort(v.begin(), v.end());
 
-	vector<int> torchers(v.begin(), v.begin()+2);
-	vector<int> people(v.begin()+2, v.end());
+    vector<int> torchers(v.begin(), v.begin()+2);
+    vector<int> people(v.begin()+2, v.end());
 
-	vector<int> torchers_left;
-	
-	bool torch_on_right = true;
+    vector<int> torchers_left;
+    
+    bool torch_on_right = true;
 
-	// move people
-	while (people.size() > 0) {
-		if (torchers.size() == 2 && torch_on_right) {
-			torchers_left.swap(torchers);
-			res += max(torchers_left[0], torchers_left[1]);
-			torch_on_right = false;
-		}
+    // move people
+    while (people.size() > 0) {
+        if (torchers.size() == 2 && torch_on_right) {
+            torchers_left.swap(torchers);
+            res += max(torchers_left[0], torchers_left[1]);
+            torch_on_right = false;
+        }
 
-		else if (!torch_on_right && torchers_left.size() == 2) {
-			if (torchers_left[0] < torchers_left[1]) {
-				torchers.push_back(torchers_left[0]);
-				res += torchers_left[0];
-				torchers_left.erase(torchers_left.begin());
-			}
-			else {
-				torchers.push_back(torchers_left[1]);
-				res += torchers_left[1];
-				torchers_left.erase(torchers_left.begin() + 1);
-			}
-			torch_on_right = true;
-		}	
+        else if (!torch_on_right && torchers_left.size() == 2) {
+            if (torchers_left[0] < torchers_left[1]) {
+                torchers.push_back(torchers_left[0]);
+                res += torchers_left[0];
+                torchers_left.erase(torchers_left.begin());
+            }
+            else {
+                torchers.push_back(torchers_left[1]);
+                res += torchers_left[1];
+                torchers_left.erase(torchers_left.begin() + 1);
+            }
+            torch_on_right = true;
+        }   
 
-		else {
-			if (torch_on_right) {
+        else {
+            if (torch_on_right) {
                 if(people.size() > 1) {
-				    res += max(people[people.size() - 1], people[people.size() - 2]);
-				    people.erase(people.begin() + people.size() - 1);
-				    people.erase(people.begin() + people.size() - 1);				    
+                    res += max(people[people.size() - 1], people[people.size() - 2]);
+                    people.erase(people.begin() + people.size() - 1);
+                    people.erase(people.begin() + people.size() - 1);                   
                 }
                 else if(people.size() == 1) {
                     res += people[0];
@@ -89,39 +89,39 @@ int get_time(vector<int>& v) {
                 }
                     
                 torch_on_right = false;
-			}
-			else {
-				if (torchers_left.size() == 1) {
-					res += torchers_left[0];
-					torchers.push_back(torchers_left[0]);
-					torchers_left.clear();
-				}
-				torch_on_right = true;
-			}
-		}
-	}
+            }
+            else {
+                if (torchers_left.size() == 1) {
+                    res += torchers_left[0];
+                    torchers.push_back(torchers_left[0]);
+                    torchers_left.clear();
+                }
+                torch_on_right = true;
+            }
+        }
+    }
 
-	// move torchers
-	while (torchers.size() > 0) {
-		if (torchers.size() == 2 && torch_on_right) {
-			res += max(torchers[0], torchers[1]);
-			torchers.clear();
-			torch_on_right = false;
-		}
-		if (torchers.size() == 1 && torchers_left.size() == 1) {
-			if (torch_on_right) {
-				res += torchers[0];
-				torchers.clear();
-				torch_on_right = false;
-			}
-			else {
-				torchers.push_back(torchers_left[0]);
-				res += torchers_left[0];
-				torch_on_right = true;
-			}
-		}
-	}
-	return res;
+    // move torchers
+    while (torchers.size() > 0) {
+        if (torchers.size() == 2 && torch_on_right) {
+            res += max(torchers[0], torchers[1]);
+            torchers.clear();
+            torch_on_right = false;
+        }
+        if (torchers.size() == 1 && torchers_left.size() == 1) {
+            if (torch_on_right) {
+                res += torchers[0];
+                torchers.clear();
+                torch_on_right = false;
+            }
+            else {
+                torchers.push_back(torchers_left[0]);
+                res += torchers_left[0];
+                torch_on_right = true;
+            }
+        }
+    }
+    return res;
 }
 
 int main(int argc, char** argv)
